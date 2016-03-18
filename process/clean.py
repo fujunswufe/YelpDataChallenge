5e8a830962978227b6c9bac91535d93178daa0fb
fujunswufe <fujunswufe@gmail.com>
2016-03-17 23:34:50 -0400

    Jun
    Update cleaning of text

--- process/clean.py
+++ process/clean.py
@@ -1,6 +1,7 @@
 __author__ = 'fujun'
 # -*- coding: utf-8 -*-
 
+import io
 import json
 from nltk import sent_tokenize
 from nltk import word_tokenize
@@ -11,7 +12,8 @@ these codes are mainly for cleaning the test. Doing sentence segmentation and cl
 
 if __name__ == "__main__":
 
-    review_file = open("yelp_academic_dataset_review.json", 'r')
+    review_file = io.open("review.json", 'r', encoding='utf8')
+    # review_file = open("review.json", 'r')
     review_clean = open("review_clean.txt", "w")
 
     for line in review_file:
@@ -19,10 +21,13 @@ if __name__ == "__main__":
         text = json_decode.get("text")
 
         sentences = sent_tokenize(text)
-        #  maybe there are some non-english words 
+        #  maybe there are some non-english words
         for sent in sentences:
             sent_tokens = word_tokenize(sent)
-            review_clean.write(sent_tokens + "\n")
+            sent_tokens.append("\n")
+            for word in sent_tokens:
+                review_clean.write(word.encode('utf8'))
+            # review_clean.write(str(sent_tokens) + "\n")
 
     review_file.close()
     review_clean.close()
